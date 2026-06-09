feff07341fe4e21f31b6dc610897d946386da79c
G705-Ghilan <g705github@email.com>
2026-06-09 11:35:12 +0300

    Add empty state example

--- src/custom_components/PageLayout.tsx
+++ src/custom_components/PageLayout.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import type { ReactNode } from "react";
 
 export default function PageLayout({ title, children }: { title: string, children?: ReactNode }) {
-    return <div className="flex flex-col w-full">
+    return <div className="flex flex-col w-full h-full">
         <div className="flex items-center  gap-3 px-4 py-4 w-full">
             <SidebarTrigger></SidebarTrigger>
             <h4>{title}</h4>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Empty, EmptyDescription, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
 import PageLayout from "@/custom_components/PageLayout";
+import { ArrowUpRightIcon, Info } from "lucide-react";
+import { NavLink } from "react-router";
 
 export default function HomePage() {
     return <PageLayout title="Home Page">
+        <div className="flex justify-center w-full mt-[5%]">
+            <Empty className="">
+                <EmptyMedia variant={'icon'}>
+                    <Info></Info>
+                </EmptyMedia>
+                <EmptyTitle>Nothing to do in Home</EmptyTitle>
+                <EmptyDescription>This project is only for learning<br />Here we can show an example of an empty state</EmptyDescription>
+                <Button
+                    variant="link"
+                    asChild
+                    className="text-muted-foreground"
+                    size="sm"
+                >
+                    <NavLink to="/react-shadcn-ui/github-search">
+                        Try Github Search <ArrowUpRightIcon />
+                    </NavLink>
+                </Button>
+            </Empty>
+        </div>
     </PageLayout>
 }
