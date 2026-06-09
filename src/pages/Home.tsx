f025592ba330a9969e86c1baf876a613e9e57cf5
G705-Ghilan <g705github@email.com>
2026-06-09 11:11:36 +0300

    remove useless stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,6 @@
-import { Button } from "@/components/ui/button";
 import PageLayout from "@/custom_components/PageLayout";
 
 export default function HomePage() {
     return <PageLayout title="Home Page">
-        <Button variant='outline' onClick={()=>{
-            localStorage.setItem("web_app_theme_mode", 'light')
-        }}>Set Theme</Button>
     </PageLayout>
 }
